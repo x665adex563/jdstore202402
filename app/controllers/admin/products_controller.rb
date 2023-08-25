d9add4f022b31f7dd53f361961748e8ac9425289
Knight~ <x665adex563@gmail.com>
2023-08-25 15:27:56 +0800

    implement backend CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,22 @@
 class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
   def new
     @product = Product.new
   end
